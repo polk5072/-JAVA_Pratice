0ea64cdf6b08f294f317e784cb9cd367c12d1c73
Yejun <polk5072@naver.com>
2023-05-09 21:52:14 +0900

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        기본적인 변수 선언
+
         int n = 15;     // 십진수 15
         int m = 015;    // 8진수 13
         int k = 0x15;   // 16진수 21
@@ -9,7 +12,48 @@ public class Main {
         long lk = 150l;
         String kor = null; // String 은 객체이므로 null이 할당가능하다.
 
-        System.out.println(lk);
+        */
+
+
+        /* 조건 연산자
+        * 조건 연산자는 3개의 피연산자로 구성되어 삼항 연산자라고도 한다.
+        * 형식 *
+
+         condition ? opr2 : opr3
+
+
+
+
+
+        int x = 5;
+        int y = 3;
+        int s = (x>y) ? 1:-2; // x > y 이므로 1이 s에 할당된다
+
+        System.out.println(s); // 1이 출력됨
+        */
+
+        /* 비트 연산
+        컴퓨터는 모든 정보를 0과 1값만 가지는 2진수로 저장한다.
+
+        여기서 2진수의 한 자리를 비트 bit 라고 부르며, 8 비트를 1byte라고 한다.
+
+        십진수 10을 한 바이트로 표현하면 00001010이다.
+        */
+
+        /*  byte flag; //byte의 8개의 각 비트는 8개의 센서 값을 각각 가리킨다.
+
+        if(flag & 0b00001000 ==0) System.out.println("온도는 0도 이하");
+        else System.out.println("온도는 0도 이상"); */
+
+        /* 비트 시프트 연산 */
+        byte a = 5; //0b00000101 로 저장됨
+        byte b = (byte) (a << 2); // a 값을 왼쪽으로 2비트만큼 이동.
+        // 즉 0b00000101 > 0b00010100
+
+        System.out.println(b); // 20이 출력됨
+
+
+
 
     }
 }
