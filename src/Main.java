7eeea0845e019d27738b00740ed573ee255c41b0
Yejun <polk5072@naver.com>
2023-05-09 21:28:46 +0900

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 15;     // 십진수 15
+        int m = 015;    // 8진수 13
+        int k = 0x15;   // 16진수 21
+        int l = 0b0101; // 2진수 5
+
+        long lk = 150l;
+        String kor = null; // String 은 객체이므로 null이 할당가능하다.
+
+        System.out.println(lk);
+
+    }
+}
